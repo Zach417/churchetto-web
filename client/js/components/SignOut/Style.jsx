3f33de350eed57ab332dcfe64a6a8b3d81cf8754
Zach417 <zallen@pension-consultants.com>
2016-03-30 10:47:02 -0500

    Updates to minor UI components.

---
+++ client/js/components/SignOut/Style.jsx
@@ -0,0 +1,34 @@
+module.exports = {
+  headerPadding: {
+    paddingTop: "65px",
+    minHeight: "100%",
+  },
+  pageContainer: {
+    color: "#3b3a36",
+    paddingTop: "65px",
+    margin: "0",
+    minHeight: "100%",
+  },
+  jumbotron: {
+    fontFamily: "'Rokkitt', serif",
+    textAlign: "center",
+    padding: "150px 0",
+    margin: "0",
+  },
+  noSpace: {
+    margin: "0",
+    padding: "0"
+  },
+  loading: {
+    textAlign: "center",
+    padding: "20px 0",
+  },
+  entitySummary: {
+    display: "block",
+    textAlign: "center",
+    padding: "20px 0",
+    backgroundColor: "#f1f4f6",
+    border: "1px solid #ccc",
+    borderRadius: "3px",
+  }
+}
