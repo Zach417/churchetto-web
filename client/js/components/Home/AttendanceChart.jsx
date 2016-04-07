bb5ada52ea0e8751e9e1f512b5a5b2f0f46ea2d1
Zach417 <zallen@pension-consultants.com>
2016-04-07 17:14:37 -0500

    Updates to Attendance Chart

--- client/js/components/Home/AttendanceChart.jsx
+++ client/js/components/Home/AttendanceChart.jsx
@@ -10,29 +10,21 @@ var ChurchStore = require('../../stores/ChurchStore');
 var Attendance = React.createClass({
   getInitialState: function () {
     return {
-      attendance: [],
+      churches: [],
     }
   },
 
   componentWillMount: function () {
+    // update to add support for multiple organizations
+    // on chart (i.e. different lines on graph)
     ChurchStore.get(function (docs) {
       if (!docs) {
         return this.setState({
-          attendance: []
+          churches: []
         });
       }
-      var result = [];
-      for (var i = 0; i < docs.length; i++) {
-        if (!docs[i].attendance) { continue; }
-        for (var j = 0; j < docs[i].attendance.length; j++) {
-          if (result.length > 5) { continue; }
-          var attendance = docs[i].attendance[j];
-          attendance.churchId = docs[i]._id;
-          result.push(attendance);
-        }
-      }
       this.setState({
-        attendance: result,
+        churches: docs,
       });
     }.bind(this));
   },
@@ -46,6 +38,13 @@ var Attendance = React.createClass({
   },
 
   render: function () {
+    if (!this.state.churches || this.state.churches.length === 0) {
+      return (
+        <div id="attendance-chart-container" style={{textAlign:"center"}}>
+          <h1 style={{margin:"5px 0"}}>{"Attendance"}</h1>
+        </div>
+      )
+    }
     return (
       <div id="attendance-chart-container" style={{textAlign:"center"}}>
         <h1 style={{margin:"5px 0"}}>{"Attendance"}</h1>
@@ -61,23 +60,73 @@ var Attendance = React.createClass({
   getChartData: function () {
     var result = {
       labels: [],
-      datasets: [{
-        label: "Church Attendance",
-        fillColor: "#ccc",
-        strokeColor: "#666666",
-        pointColor: "#f4f4f4",
-        pointStrokeColor: "#666666",
-        pointHighlightFill: "#f4f4f4",
-        pointHighlightStroke: "#3c948b",
-        data: []
-      }]
+      datasets: []
     };
-    this.state.attendance.sort(function (a, b) {
-      return new Date(a.date) - new Date(b.date);
-    }).map(function (doc, i) {
-      result.labels[i] = moment(doc.date).format("MM/DD/YYYY");
-      result.datasets[0].data[i] = doc.count;
+
+    // set labels
+    this.state.churches.map(function (church, i) {
+      church.attendance.map(function (attendance, j) {
+        var exists = false;
+        for (var k = 0; k < result.labels; k++) {
+          if (result.labels[k] === moment(attendance.date).format("MM/DD/YYYY")) {
+            exists = true;
+          }
+        }
+        if (!exists) {
+          result.labels.push(moment(attendance.date).format("MM/DD/YYYY"));
+        }
+      });
     });
+
+    result.labels = result.labels.sort(function (a, b) {
+      return new Date(a) - new Date(b);
+    });
+
+    var datasets = [];
+    datasets[0] = {
+      fillColor: "rgba(204,204,204,0.2)",
+      strokeColor: "#666666",
+      pointColor: "#f4f4f4",
+      pointStrokeColor: "#666666",
+      pointHighlightFill: "#f4f4f4",
+      pointHighlightStroke: "#3c948b",
+      data: []
+    }
+    datasets[1] = {
+      fillColor: "rgba(230,230,230,0.2)",
+      strokeColor: "#666666",
+      pointColor: "#f4f4f4",
+      pointStrokeColor: "#666666",
+      pointHighlightFill: "#f4f4f4",
+      pointHighlightStroke: "#3c948b",
+      data: []
+    }
+
+    this.state.churches.map(function (church, i) {
+      var label = "Church " + i;
+      if (church.name) {
+        label = church.name;
+      }
+      if (!datasets[i]) {
+        result.datasets[i] = datasets[0];
+        result.datasets[i].label = label;
+      } else {
+        result.datasets[i] = datasets[i];
+        result.datasets[i].label = label;
+      }
+      church.attendance.map(function (attendance, j) {
+        result.labels.map(function (label, k) {
+          if (!result.datasets[i].data[k]) {
+            if (result.labels[k] === moment(attendance.date).format("MM/DD/YYYY")) {
+              result.datasets[i].data[k] = attendance.count;
+            } else {
+              result.datasets[i].data[k] = null;
+            }
+          }
+        });
+      });
+    });
+    console.log(result);
     return result;
   },
 
